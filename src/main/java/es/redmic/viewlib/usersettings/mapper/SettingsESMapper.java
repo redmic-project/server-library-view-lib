768054b98745cbeaa7636873aec33b7580de2516
Noel Alonso <noel@redmic.es>
2019-07-11 10:03:21 +0100

    Añade mapping dto a model y model a dto + tests

---
+++ src/main/java/es/redmic/viewlib/usersettings/mapper/SettingsESMapper.java
@@ -0,0 +1,37 @@
+package es.redmic.viewlib.usersettings.mapper;
+
+/*-
+ * #%L
+ * view-lib
+ * %%
+ * Copyright (C) 2019 REDMIC Project / Server
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import org.mapstruct.Mapper;
+
+import es.redmic.usersettingslib.dto.SettingsDTO;
+import es.redmic.usersettingslib.model.Settings;
+import es.redmic.viewlib.common.mapper.es2dto.DataCollectionESMapper;
+
+@Mapper
+// @ConditionalOnProperty(name = "redmic.user-settings.enabled", havingValue =
+// "true")
+public abstract class SettingsESMapper extends DataCollectionESMapper<SettingsDTO, Settings> {
+
+	public abstract SettingsDTO map(Settings model);
+
+	public abstract Settings map(SettingsDTO dto);
+}
