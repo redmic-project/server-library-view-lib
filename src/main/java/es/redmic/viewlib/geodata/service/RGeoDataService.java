bc02fdf5ddb5a6437dfeee7606198c78e7241628
Noel <noelalonso1986@gmail.com>
2021-11-30 12:29:05 +0000

    Actualiza dependencia model
    Arregla problemas de compatibilidad por cambios en la dependencia

--- src/main/java/es/redmic/viewlib/geodata/service/RGeoDataService.java
+++ src/main/java/es/redmic/viewlib/geodata/service/RGeoDataService.java
@@ -9,9 +9,9 @@ package es.redmic.viewlib.geodata.service;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,7 +25,7 @@ import java.util.List;
 import es.redmic.brokerlib.avro.geodata.common.FeatureDTO;
 import es.redmic.models.es.common.query.dto.MgetDTO;
 import es.redmic.models.es.common.query.dto.SimpleQueryDTO;
-import es.redmic.models.es.geojson.base.Feature;
+import es.redmic.models.es.geojson.common.model.Feature;
 import es.redmic.models.es.geojson.common.dto.GeoJSONFeatureCollectionDTO;
 import es.redmic.models.es.geojson.wrapper.GeoHitWrapper;
 import es.redmic.models.es.geojson.wrapper.GeoHitsWrapper;
